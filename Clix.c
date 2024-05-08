5f9c3e3cd0f0a45a328b29346220625bde2df972
Amin Ehterami <A.Ehterami@proton.me>
2024-05-08 15:47:32 +0330

    fix minor problems fix Clix version show for empty file

--- Clix.c
+++ Clix.c
@@ -1,3 +1,6 @@
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -224,16 +227,28 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 /* this function is for opening and reading a file from disk*/
-void editorOpen() {
-  // TODO : hardcoded entry string for now.
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
-  //
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 0;
+void editorOpen(char *filename) {
+  // open file
+  FILE *fp = fopen(filename, "r");
+  if (!fp)
+    die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 &&
+           (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+      linelen--;
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
 }
 
 /*** append buffer ***/
@@ -269,9 +284,9 @@ void editorDrawRows(struct abuf *ab) {
   int y;
 
   for (y = 0; y < E.screenrows; y++) {
-    if (y > E.numrows) {
-      if (y == E.screenrows / 3) {
-        //printing Clix Editor Version for empty editor 
+    if (y >= E.numrows) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
+        // printing Clix Editor Version for empty editor
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
                                   "Clix editor -- version %s", CLIX_VERSION);
@@ -389,10 +404,12 @@ void editorProcessKeypress() {
   }
   }
 }
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
   while (1) {
     editorRefreshScreen();
@@ -400,4 +417,4 @@ int main() {
   }
   return 0;
 }
-// step 58
+// step 59
