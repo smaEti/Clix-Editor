854dba6708f316f6600c26a7409f4cec5ee0677e
Amin Ehterami <A.Ehterami@proton.me>
2024-05-09 12:11:46 +0330

    print multiple lines (from file)

--- Clix.c
+++ Clix.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #define _DEFAULT_SOURCE
 #define _BSD_SOURCE
 #define _GNU_SOURCE
@@ -43,7 +44,7 @@ struct editorConfig {
   int screenrows;
   int screencols;
   int numrows;
-  erow row;
+  erow *row;
   struct termios original_termios;
 };
 
@@ -226,6 +227,20 @@ int getWindowSize(int *rows, int *cols) {
     return 0;
   }
 }
+/*
+allocates string for each row and writes the each row in it;
+*/
+void editorAppendRow(char *s, size_t len) {
+  // allocating the rows
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
+}
+
 /* this function is for opening and reading a file from disk*/
 void editorOpen(char *filename) {
   // open file
@@ -236,17 +251,14 @@ void editorOpen(char *filename) {
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
-  linelen = getline(&line, &linecap, fp);
-  if (linelen != -1) {
+  //reads every row of file and calls editorAppendRow for everyline to append the row in buffer
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
     while (linelen > 0 &&
            (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
       linelen--;
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    editorAppendRow(line, linelen);
   }
+
   free(line);
   fclose(fp);
 }
@@ -275,6 +287,7 @@ void initEditor() {
   E.cx = 0;
   E.cy = 0;
   E.numrows = 0;
+  E.row = NULL;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
@@ -282,11 +295,13 @@ void initEditor() {
 /*prints ~ in the start of every line (the commented section is for numbers)*/
 void editorDrawRows(struct abuf *ab) {
   int y;
-
+  // looping on each row
   for (y = 0; y < E.screenrows; y++) {
     if (y >= E.numrows) {
+      // if there is not any numrows (files) shows welcome page otherwise it
+      // prints ~ to console
       if (E.numrows == 0 && y == E.screenrows / 3) {
-        // printing Clix Editor Version for empty editor
+        // Wellcome page :
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
                                   "Clix editor -- version %s", CLIX_VERSION);
@@ -300,15 +315,16 @@ void editorDrawRows(struct abuf *ab) {
         while (padding--)
           abAppend(ab, " ", 1);
         abAppend(ab, welcome, welcomelen);
-      } else {
+
+      } else { // OTHERWISE
         // appends ~ to the buffer we made
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row.size;
+      int len = E.row[y].size;
       if (len > E.screencols)
         len = E.screencols;
-      abAppend(ab, E.row.chars, len);
+      abAppend(ab, E.row[y].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
