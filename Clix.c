f660fcecbd8e2a4e9d5cae3dfdad6bf2825ec2ba
Amin Ehterami <A.Ehterami@proton.me>
2024-05-10 19:05:54 +0330

    limit horizontal scrolling

--- Clix.c
+++ Clix.c
@@ -380,7 +380,8 @@ void editorRefreshScreen() {
 
   // setting the first position of cursor in screen
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx + E.coloff) + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+           (E.cx + E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -389,6 +390,8 @@ void editorRefreshScreen() {
   abFree(&ab);
 }
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
   case ARROW_LEFT:
     if (E.cx != 0) {
@@ -396,7 +399,9 @@ void editorMoveCursor(int key) {
     }
     break;
   case ARROW_RIGHT:
+    if (row && E.cx < row->size) {
       E.cx++;
+    }
     break;
   case ARROW_UP:
     if (E.cy != 0) {
